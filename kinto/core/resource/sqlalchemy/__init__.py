c3c92500660d24913a434229b57bf778062ff21c
Mariano Mara <mmara@onapsis.com>
2016-05-28 07:46:24 -0300

    Append preparers and validators automatically

--- kinto/core/resource/sqlalchemy/__init__.py
+++ kinto/core/resource/sqlalchemy/__init__.py
@@ -1,13 +1,14 @@
 import datetime
 
-from .. import logger
-from colanderalchemy import SQLAlchemySchemaNode, setup_schema
+import colander
+from colanderalchemy import SQLAlchemySchemaNode
 from pyramid_sqlalchemy import BaseObject, metadata
 from sqlalchemy import Column, event
 from sqlalchemy import String, Boolean, Integer, BigInteger
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import mapper
 
+from .. import logger
 from ...resource import ShareableResource, ResourceSchema
 
 key = SQLAlchemySchemaNode.sqla_info_key
@@ -41,9 +42,10 @@ class SQLABaseObject(object):
         return self.last_modified.replace(tzinfo=datetime.timezone.utc).timestamp()
 
     @classmethod
-    def deserialize(cls, cstruct):
+    def deserialize(cls, cstruct, context=None):
         try:
-            return cls.__schema__.deserialize(cstruct)
+            appstruct = cls.__schema__.deserialize(cstruct)
+            return cls.__schema__.objectify(appstruct, context)
         except AttributeError:
             raise NoSchemaException(cls)
 
@@ -61,6 +63,14 @@ class SQLABaseObject(object):
     def __repr_attributes(self):
         return dict([(att.name, getattr(self, att.name)) for att in self.__schema__.children
                                                                                if hasattr(att, 'repr') and att.repr])
+
+    @staticmethod
+    def global_preparer(value):
+        return value
+
+    @staticmethod
+    def global_validator(node, value):
+        return True
 
 
 class SQLASchemaResource(SQLAlchemySchemaNode, ResourceSchema):
@@ -80,13 +90,80 @@ class SQLAUserResource(ShareableResource):
 
 
 def schema_setup(mapper, klass):
+    """
+    Evaluate if klass has a __schema__ attribute already attached. In case it does not, create it and later
+    evaluate preparers and validators
+    """
     if not hasattr(klass, '__schema__'):
         klass.__schema__ = SQLASchemaResource(klass)
-        for child in klass.__schema__.children:
-            if isinstance(child.preparer, str):
-                child.preparer = getattr(klass, child.preparer, None)
-            if isinstance(child.validator, str):
-                child.validator = getattr(klass, child.validator, None)
+        MethodAppender(klass).append_methods()
+
+
+class MethodAppender(object):
+
+    methods = ('preparer', 'validator')
+
+    def __init__(self, klass):
+        self.klass = klass
+
+    def append_methods(self):
+        """
+        Evaluate if default preparers and validators have to be attach, first in the class and then in each child
+        """
+        self._conversion(self.klass.__schema__, 'global')
+        for child in self.klass.__schema__.children:
+            self._conversion(child, child.name)
+
+    def _conversion(self, pointer, keyword):
+        """
+        For each pointer, append preparers and validators in case they exist
+        """
+        self._append_preparer(pointer, keyword)
+        self._append_validator(pointer, keyword)
+
+    def _append_preparer(self, pointer, keyword):
+        """
+        If preparer is null, append global default preparer for class or attribute. If it is a string, search
+        for such method in class.
+        """
+        if pointer.preparer is None:
+            pointer.preparer = getattr(self.klass, '{}_preparer'.format(keyword), None)
+        elif isinstance(pointer.preparer, str):
+            pointer.preparer = getattr(self.klass, pointer.preparer, None)
+
+    def _append_validator(self, pointer, keyword):
+        """
+        Evaluate _validator_ attribute with the following rules:
+         * append default global validator or attribute validator in case they exist to global list
+         * if defined validator is a string, look for it in the klass methods and append to global list
+         * if validator is a list, append them to global list (iterating them in case some of them are strings to
+           be located)
+         * if they are already methods append them to global list
+        Once the list has been collected, _set_method will evaluate it and transform to the right thing
+        """
+        methods_list = [getattr(self.klass, '{keyword}_validator'.format(keyword=keyword), None)]
+        if isinstance(pointer.validator, str):
+            methods_list.append(getattr(self.klass, pointer.validator))
+        elif hasattr(pointer.validator, '__iter__'):
+            for each in pointer.validator:
+                if isinstance(each, str):
+                    methods_list.append(getattr(self.klass, each, None))
+                else:
+                    methods_list.append(each)
+        elif pointer.validator is not None:
+            methods_list.append(pointer.validator)
+        self._set_method(pointer, methods_list)
+
+    def _set_method(self, pointer, methods_list):
+        """
+        Remove null items from list of methods and in case only one method remain, append it directly as validator.
+        Otherwise wrap them with Colander.All()
+        """
+        clean_list = [i for i in methods_list if i is not None]
+        if len(clean_list) == 1:
+            pointer.validator = clean_list[0]
+        elif len(clean_list) > 1:
+            pointer.validator = colander.All(*clean_list)
 
 
 event.listen(mapper, 'mapper_configured', schema_setup)
